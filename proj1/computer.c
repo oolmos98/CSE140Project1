e97b1c595d918158ae8006403a0b6f27785bcdf0
Omar Olmos <oolmos@ucmerced.edu>
2019-10-01 15:51:45 -0700

    Removed unused variables and added almost all executions!

--- proj1/computer.c
+++ proj1/computer.c
@@ -23,30 +23,20 @@ RegVals rVals;
 /*  
 	opcodes for I-format
 */
-const unsigned int 	addi = 0x08,
-					addiu = 0x09,
-					beq = 0x04,
-					blez = 0x06,
-					bne = 0x05,
-					bgtz = 0x07,
-					lb = 0x20,
-					lbu = 0x24,
-					lhu = 0x25,
-					lui = 0x0F,
-					lw = 0x23,
-					ori = 0x0D,
-					sb = 0x28,
-					sh = 0x29,
-					slti = 0x0A,
-					sltiu = 0x0B,
-					sw = 0x2B;
+const unsigned int andi = 0x0C,
+									 addiu = 0x09,
+									 beq = 0x04,
+									 bne = 0x05,
+									 bgtz = 0x07,
+									 lui = 0x0F,
+									 lw = 0x23,
+									 ori = 0x0D,
+									 sw = 0x2B;
 /*  
 	opcodes for J-format
 */
-const unsigned int 	jal = 0x03,
-					jump = 0x02;
-
-
+const unsigned int jal = 0x03,
+									 jump = 0x02;
 
 /*
 
@@ -54,43 +44,31 @@ const unsigned int 	jal = 0x03,
 
 */
 
-const unsigned int 	add = 0x20,
-					addu = 0x21,
-					and = 0x24,
-					i_div = 0x1A,			// Another 'div' already exists oof
-					divu = 0x1B,
-					jr = 0x08,
-					mfhi = 0x10,
-					mthi = 0x11,
-					mflo = 0x12,
-					mtlo = 0x13,
-					mult = 0x18,
-					multu = 0x19,
-					nor = 0x27,
-					xor = 0x26,
-					or = 0x25,
-					slt = 0x2A,
-					sltu = 0x2B,
-					sll = 0x00,		// uses shamt
-					srl = 0x02,		// uses shamt
-					sra = 0x03,
-					sub = 0x22,
-					subu = 0x23;
-
+const unsigned int
+		addu = 0x21,
+		and = 0x24,
+		jr = 0x08,
+		or = 0x25,
+		slt = 0x2A,
+		sll = 0x00, // uses shamt
+		srl = 0x02, // uses shamt
+		subu = 0x23;
 
 /*
 
 	Implementing Control
 
 */
-enum ALUctlInputs {
+enum ALUctlInputs
+{
 	ADD = 0,
 	SUB,
 	OR,
 	AND
 };
 
-enum RegDst {
+enum RegDst
+{
 	Rt = 0,
 	Rd
 };
@@ -101,12 +79,12 @@ enum RegDst {
 
 */
 
-int ALUctl = -1; 	// determined by the ALUctlInputs
+int ALUctl = -1; // determined by the ALUctlInputs
 int RegDst = -1;
-int ALUsrc = -1;	// 0 for an register input, 1 for an immediate input
+int ALUsrc = -1; // 0 for an register input, 1 for an immediate input
 int ExtOp = -1;
 int MemWr = -1;
-int MemToReg = -1;	//
+int MemToReg = -1; //
 
 /*
  *  Return an initialized computer with the stack pointer set to the
@@ -115,7 +93,7 @@ int MemToReg = -1;	//
  *  The other arguments govern how the program interacts with the user.
  */
 void InitComputer(FILE *filein, int printingRegisters, int printingMemory,
-				  int debugging, int interactive)
+									int debugging, int interactive)
 {
 	int k;
 	unsigned int instr;
@@ -205,7 +183,7 @@ void Simulate()
 
 		UpdatePC(&d, val);
 
-	/* 
+		/* 
 	 * Perform memory load or store. Place the
 	 * address of any updated memory in *changedMem, 
 	 * otherwise put -1 in *changedMem. 
@@ -246,7 +224,7 @@ void PrintInfo(int changedReg, int changedMem)
 	else if (!mips.printingRegisters)
 	{
 		printf("Updated r%2.2d to %8.8x\n",
-			   changedReg, mips.registers[changedReg]);
+					 changedReg, mips.registers[changedReg]);
 	}
 	else
 	{
@@ -266,15 +244,15 @@ void PrintInfo(int changedReg, int changedMem)
 	else if (!mips.printingMemory)
 	{
 		printf("Updated memory at address %8.8x to %8.8x\n",
-			   changedMem, Fetch(changedMem));
+					 changedMem, Fetch(changedMem));
 	}
 	else
 	{
 		printf("Nonzero memory\n");
 		printf("ADDR	  CONTENTS\n");
 		for (addr = 0x00400000 + 4 * MAXNUMINSTRS;
-			 addr < 0x00400000 + 4 * (MAXNUMINSTRS + MAXNUMDATA);
-			 addr = addr + 4)
+				 addr < 0x00400000 + 4 * (MAXNUMINSTRS + MAXNUMDATA);
+				 addr = addr + 4)
 		{
 			if (Fetch(addr) != 0)
 			{
@@ -316,13 +294,12 @@ void Decode(unsigned int instr, DecodedInstr *d, RegVals *rVals)
 
 	*/
 
-	ALUctl = -1; 	// determined by the ALUctlInputs
+	ALUctl = -1; // determined by the ALUctlInputs
 	RegDst = -1;
-	ALUsrc = -1;	// 0 for an register input, 1 for an immediate input
+	ALUsrc = -1; // 0 for an register input, 1 for an immediate input
 	ExtOp = -1;
 	MemWr = -1;
-	MemToReg = -1;	//
-
+	MemToReg = -1; //
 
 	if (opcode == 0)
 		format = 'R';
@@ -333,64 +310,54 @@ void Decode(unsigned int instr, DecodedInstr *d, RegVals *rVals)
 
 	switch (format)
 	{
-		case 'R': //R Format
-			/*
+	case 'R': //R Format
+		/*
 				R-format
 				opcode, rs, rt, rd, shamt, funct
 			*/
-			d->op = 0;
-			d->regs.r.rs = rVals->R_rs = (instr << 6) >> 27;
-			d->regs.r.rt = rVals->R_rt = (instr << 11) >> 27;
-			d->regs.r.rd = rVals->R_rd = (instr << 16) >> 27;
-			d->regs.r.shamt = (instr << 21) >> 27;
-			d->regs.r.funct = (instr << 26) >> 26;
-			//printf("%d\n", (instr << 26) >> 26);
-			//PrintInstruction(d);
-			if(d->regs.r.funct == jr) 
-			{
-				/*
-					(mips.pc + 4) << 4
-					iiiiiiiiiiiiiiiiiiiiiiiiiiii0000
+		d->type = R;
+		d->op = 0;
+		d->regs.r.rs = rVals->R_rs = (instr << 6) >> 27;
+		d->regs.r.rt = rVals->R_rt = (instr << 11) >> 27;
+		d->regs.r.rd = rVals->R_rd = (instr << 16) >> 27;
+		d->regs.r.shamt = (instr << 21) >> 27;
+		d->regs.r.funct = (instr << 26) >> 26;
+		//printf("%d\n", (instr << 26) >> 26);
+		//PrintInstruction(d);
+		break;
 
-					((mips.pc + 4) << 4) >> 6
-					000000iiiiiiiiiiiiiiiiiiiiiiiiii
-				*/
-				d->regs.r.rs = ((mips.pc + 4) << 4) >> 6;
-				UpdatePC(d, mips.registers[31]);
-			}
-			break;
-
-		case 'I':
-			/*
+	case 'I':
+		/*
 				I-format
 				opcode, rs, rt, immediate
 			*/
-			d->op = opcode;
-			//printf("I: %d\n", opcode);
-			d->regs.i.rs = (instr << 6) >> 27;
-			d->regs.i.rt = (instr << 11) >> 27;
-			d->regs.i.addr_or_immed = (instr << 16) >> 16;
+		d->type = I;
+		d->op = opcode;
+		//printf("I: %d\n", opcode);
+		d->regs.i.rs = rVals->R_rs = (instr << 6) >> 27;
+		d->regs.i.rt = rVals->R_rt = (instr << 11) >> 27;
+		d->regs.i.addr_or_immed = (instr << 16) >> 16;
 
-			// ALUsrc will always be in Immediate field
-			ALUsrc = 1;
+		// ALUsrc will always be in Immediate field
+		ALUsrc = 1;
 
-			/*
+		/*
 				An example.
 			*/
-			switch(opcode)
-			{
-				case lw:
-					ALUctl = ADD;
-					MemWr = 1;
-					MemToReg = 1;
-					ExtOp = 1; // Signed
-					RegDst = Rt;
-				break;
-			}
-
+		switch (opcode)
+		{
+		case lw:
+			ALUctl = ADD;
+			MemWr = 1;
+			MemToReg = 1;
+			ExtOp = 1; // Signed
+			RegDst = Rt;
 			break;
-		case 'J': 
-			/*
+		}
+
+		break;
+	case 'J':
+		/*
 				I-format
 				opcode, jump_addr
 
@@ -415,32 +382,21 @@ void Decode(unsigned int instr, DecodedInstr *d, RegVals *rVals)
 
 
 			*/
+		d->type = J;
+		d->op = opcode;
+		int curr_pc = (mips.pc >> 28) << 28;
 
-			d->op = opcode;
-			int curr_pc = (mips.pc >> 28) << 28;
+		int target_pc = ((instr << 6) >> 4) | curr_pc;
 
-			int target_pc = ((instr << 6) >> 4) | curr_pc;
+		d->regs.j.target = target_pc;
 
+		//PrintInstruction(d);
+		//UpdatePC(d, d->regs.j.target);
+		break;
 
-			d->regs.j.target = target_pc;
+	default:
 
-			if(opcode == jal || opcode == jump )
-			{
-				if(opcode == jal){
-					mips.registers[31] = mips.pc+=4;
-				}
-				UpdatePC(d, target_pc);
-
-			}
-
-			
-			//PrintInstruction(d);
-			//UpdatePC(d, d->regs.j.target);
-			break;
-
-		default:
-
-			break;
+		break;
 	}
 }
 
@@ -452,224 +408,128 @@ void PrintInstruction(DecodedInstr *d)
 {
 	/* Your code goes here */
 	// Check if the instruction is R-foarmat
-	char* instr = (char*)malloc(sizeof(char)*5);
+	char *instr = (char *)malloc(sizeof(char) * 5);
 	int supported_instr = 1;
-
 
 	//printf("%d\n", d->op);
 	//printf("R: %d\n", d->regs.r.funct);
 
-	if(d->op == 0)
+	if (d->op == 0)
 	{
 		//printf("R: %d\n", d->regs.r.funct);
-		switch(d->regs.r.funct) 
+		switch (d->regs.r.funct)
 		{
-			case add:
-				strcpy(instr, "add");
+		case addu:
+			strcpy(instr, "addu");
 			break;
 
-			case addu:
-				strcpy(instr, "addu");
+		case and:
+			strcpy(instr, "and");
 			break;
 
-			case and:
-				strcpy(instr, "and");
+		case jr:
+			strcpy(instr, "jr");
 			break;
 
-			case i_div:
-				strcpy(instr, "div");
+		case or:
+			strcpy(instr, "or");
 			break;
 
-			case divu:
-				strcpy(instr, "divu");
+		case slt:
+			strcpy(instr, "slt");
 			break;
 
-			case jr:
-				strcpy(instr, "jr");
+		case sll:
+			strcpy(instr, "sll");
 			break;
 
-			case mfhi:
-				strcpy(instr, "mfhi");
+		case srl:
+			strcpy(instr, "srl");
 			break;
 
-			case mthi:
-				strcpy(instr, "mthi");
+		case subu:
+			strcpy(instr, "subu");
 			break;
 
-			case mflo:
-				strcpy(instr, "mflo");
-			break;
-
-			case mtlo:
-				strcpy(instr, "mtlo");
-			break;
-
-			case mult:
-				strcpy(instr, "mult");
-			break;
-
-			case multu:
-				strcpy(instr, "multu");
-			break;
-
-			case nor:
-				strcpy(instr, "nor");
-			break;
-
-			case xor:
-				strcpy(instr, "xor");
-			break;
-
-			case or:
-				strcpy(instr, "or");
-			break;
-
-			case slt:
-				strcpy(instr, "slt");
-			break;
-
-			case sltu:
-				strcpy(instr, "sltu");
-			break;
-
-			case sll:
-				strcpy(instr, "sll");
-			break;
-
-			case srl:
-				strcpy(instr, "srl");
-			break;
-
-			case sra:
-				strcpy(instr, "sra");
-			break;
-
-			case sub:
-				strcpy(instr, "sub");
-			break;
-
-			case subu:
-				strcpy(instr, "subu");
-			break;
-
-			default: // gets triggered if theres an unsupported code
-				supported_instr = 0;
+		default: // gets triggered if theres an unsupported code
+			supported_instr = 0;
 			break;
 		}
 	}
-	else 
+	else
 	{
-		switch(d->op)
+		switch (d->op)
 		{
 			//printf("%d\n", d->op);
-			case addi:
-				strcpy(instr, "addi");
+
+		case addiu:
+			strcpy(instr, "addiu");
 			break;
 
-			case addiu:
-				strcpy(instr, "addiu");
+		case beq:
+			strcpy(instr, "beq");
 			break;
 
-			case beq:
-				strcpy(instr, "beq");
+		case bne:
+			strcpy(instr, "bne");
 			break;
 
-			case blez:
-				strcpy(instr, "blez");
+		case bgtz:
+			strcpy(instr, "bgtz");
 			break;
 
-			case bne:
-				strcpy(instr, "bne");
+		case lui:
+			strcpy(instr, "lui");
 			break;
 
-			case bgtz:
-				strcpy(instr, "bgtz");
+		case lw:
+			strcpy(instr, "lw");
 			break;
 
-			case lb:
-				strcpy(instr, "lb");
+		case ori:
+			strcpy(instr, "ori");
 			break;
 
-			case lbu:
-				strcpy(instr, "lbu");
+		case sw:
+			strcpy(instr, "sw");
 			break;
 
-			case lhu:
-				strcpy(instr, "lhu");
+		case jal:
+			strcpy(instr, "jal");
 			break;
 
-			case lui:
-				strcpy(instr, "lui");
+		case jump:
+			strcpy(instr, "j");
 			break;
 
-			case lw:
-				strcpy(instr, "lw");
+		default: // gets triggered if theres an unsupported code
+			supported_instr = 0;
 			break;
-
-			case ori:
-				strcpy(instr, "ori");
-			break;
-
-			case sb:
-				strcpy(instr, "sb");
-			break;
-
-			case sh:
-				strcpy(instr, "sh");
-			break;
-
-			case slti:
-				strcpy(instr, "slti");
-			break;
-
-			case sltiu:
-				strcpy(instr, "sltiu");
-			break;
-
-			case sw:
-				strcpy(instr, "sw");
-			break;
-
-			case jal:
-				strcpy(instr, "jal");
-			break;
-
-			case jump:
-				strcpy(instr, "j");
-			break;
-
-			default: // gets triggered if theres an unsupported code
-				supported_instr = 0;
-			break;
-
 		}
 	}
 
-	if(supported_instr == 0) {
+	if (supported_instr == 0)
+	{
 		printf("Unsupported instruction found. Terminating program\n");
 		exit(0);
 	}
-	
 
-	if(d->type == R) 
+	if (d->type == R)
 	{
-		if(d->regs.r.funct == sll || d->regs.r.funct == srl)
+		if (d->regs.r.funct == sll || d->regs.r.funct == srl)
 		{
 			printf("%s $%d, $%d, %d\n", instr, d->regs.r.rd, d->regs.r.rs, d->regs.r.shamt);
 		}
-		else 
+		else
 		{
 			printf("%s $%d, $%d, $%d\n", instr, d->regs.r.rd, d->regs.r.rs, d->regs.r.rt);
 		}
-		
 	}
-	else if(d->type == I) 
+	else if (d->type == I)
 	{
-
-	} 
-	else if(d->type == J)
+	}
+	else if (d->type == J)
 	{
-
 	}
 
 	free(instr);
@@ -681,103 +541,92 @@ int Execute(DecodedInstr *d, RegVals *rVals)
 	/* Your code goes here */
 
 	// if its an R-format
-	if(d->op == 0)
+	if (d->type == R)
 	{
-		switch(ALUctl)
+		switch (d->regs.r.funct)
 		{
-			case ADD:	// For ADD, branch etc.
 
-			break;
+		case addu:
+			rVals->R_rd = rVals->R_rs + rVals->R_rt;
+			return 0;
 
-			case SUB:
+		case subu:
+			rVals->R_rd = rVals->R_rs - rVals->R_rt;
+			return 0;
 
+		case sll:
+			rVals->R_rd = rVals->R_rt << d->regs.r.shamt;
+			return 0;
 
+		case srl:
+			rVals->R_rd = rVals->R_rt >> d->regs.r.shamt;
+			return 0;
 
-			break;
+		case and:
+			rVals->R_rd = rVals->R_rs & rVals->R_rt;
+			return 0;
 
-			case OR:	// For OR , NOR, and XOR instruction
-				// Or: rd = rs | rt
-				rVals->R_rt = d->regs.r.rt;
-				rVals->R_rs = d->regs.r.rs;
+		case or:
+			rVals->R_rd = rVals->R_rs | rVals->R_rt;
+			return 0;
 
-				switch(d->regs.r.funct)
-				{
-					case or:
-						rVals->R_rd = rVals->R_rs | rVals->R_rt;
-					break;
+		case slt:
+			rVals->R_rd = (rVals->R_rs < rVals->R_rt) ? 1 : 0;
+			return 0;
 
-					case nor:
-						rVals->R_rd = ~(rVals->R_rs | rVals->R_rt);
-					break;
-
-					case xor:
-
-					break;
-				}
-			break;
-
-			case AND: // For AND instruction
-				rVals->R_rt = d->regs.r.rt;
-				rVals->R_rs = d->regs.r.rs;
-				rVals->R_rd = rVals->R_rs & rVals->R_rt;
-			break;
+		case jr:
+			return mips.registers[31];
 		}
 	}
-	else 
+	if (d->type == I)
 	{
-
-		switch(ALUctl)
+		switch (d->op)
 		{
-			case ADD:	// For ADD, branch etc.
 
-			break;
+		case addiu:
+			rVals->R_rt = rVals->R_rs + d->regs.i.addr_or_immed;
+			return 0;
 
-			case SUB:
-				if(d->op == beq || d->op == bne) {
-					rVals->R_rt = d->regs.i.rt;
-					rVals->R_rs = d->regs.i.rs;
+		case andi:
+			rVals->R_rt = rVals->R_rs & d->regs.i.addr_or_immed;
+			return 0;
 
-					result = rVals->R_rt - rVals->R_rs;
-					if(result == 0 )
-					{
-						//UpdatePC();
-					}
-					else if(d->op == bne)
-					{
-						//UpdatePC();
-					}
-				}
-				
-			break;
+		case ori:
+			rVals->R_rt = rVals->R_rs | d->regs.i.addr_or_immed;
+			return 0;
+		case lui:
+			return 0;
 
-			case OR:	// For OR , NOR, and XOR instruction
-				// Or: rd = rs | rt
-				rVals->R_rt = d->regs.i.rt;
-				rVals->R_rs = d->regs.i.rs;
-				int immediate = d->regs.i.addr_or_immed;
+		case beq:
+			if (rVals->R_rt == rVals->R_rs)
+			{
+				return d->regs.i.addr_or_immed;
+			}
+			return 0;
 
-				switch(d->regs.r.funct)
-				{
-					case or:
-						rVals->R_rt = rVals->R_rs | addr_or_immed;
-					break;
+		case bne:
+			if (rVals->R_rt != rVals->R_rs)
+			{
+				return d->regs.i.addr_or_immed;
+			}
+			return 0;
 
-					case nor:
-						rVals->R_rt = ~(rVals->R_rs | addr_or_immed);
-					break;
+		case lw:
+			return 0;
 
-					case xor:
-
-					break;
-				}
-			break;
-
-			case AND: // For AND instruction
-				rVals->R_rt = d->regs.r.rt;
-				rVals->R_rs = d->regs.r.rs;
-				rVals->R_rd = rVals->R_rs & rVals->R_rt;
-			break;
+		case sw:
+			return 0;
 		}
+	}
+
+	if (d->op == jal || d->op == jump)
+	{
+		if (d->op == jal)
+		{
+			mips.registers[31] = mips.pc;
+			return d->regs.j.target;
+		}
+		return d->regs.j.target;
 	}
 
 	return 0;
@@ -794,13 +643,13 @@ void UpdatePC(DecodedInstr *d, int val)
 	mips.pc += 4;
 	/* Your code goes here */
 
-	if(d->op == jump || d->op == jal) 
-	{
-		mips.pc = d->regs.j.target;
-	}
-	else if (d->op == 0 && d->regs.r.funct == jr) 
+	if (d->op == jump || d->op == jal)
 	{
 		mips.pc = val;
+	}
+	else if (d->op == 0 && d->regs.r.funct == jr)
+	{
+		mips.pc = mips.registers[31];
 	}
 }
 
